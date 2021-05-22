ff04736b668a8bc330555f09feeaf967473588f2
KevinHenleyCode <kevinhenleyinfo@gmail.com>
2021-05-22 01:14:04 -0500

    merged both js files together

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const mysql = require('mysql')
 const fs = require('fs')
 const inquirer = require('inquirer')
-const question = require('./js/index')
+const { userInfo } = require('os')
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -16,9 +16,24 @@ const connection = mysql.createConnection({
 
 connection.connect((err) => {
     if (err) throw err;
-    console.log(`Connected as ID: ${connection.threadId}.`);
-    createTables()
+    console.log(`Connected as ID: ${connection.threadId}.\n`);
+    createDatabase()
 })
+
+
+const createDatabase = () => {
+    connection.query(`
+    CREATE DATABASE IF NOT EXISTS employee_database;
+    `, (err, res) => {
+
+        if (err) throw err;
+    })
+    
+    console.log(`Created employee_database.\n`);
+    createTables()
+}
+
+
 
 
 const createTables = () => {
@@ -54,8 +69,108 @@ const createTables = () => {
             if (err) throw err;
         })
         
-        console.log(`Tables created!`);
-        question.qStart()
+        console.log(`Tables created!\n`);
+        qStart()
         connection.end()
 }
 
+    
+const qStart = () => {
+
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'mainMenu',
+            message: 'How may I help you?',
+            choices: ['Add Info', 'Delete Info', 'View Info', 'Exit']
+        },
+        {
+            type: 'input',
+            name: 'next',
+            message: 'What can I do next?'
+        }
+    ])
+    .then((userInput) => {
+        switch (userInput.mainMenu) {
+            case 'Add Info':
+                console.log(`Okay let's Add some info!!`);
+                qAdd()
+                break;
+            case 'Delete Info':
+                console.log(`Okay let's Delete some info!!`);
+                qDelete()
+                break;
+            case 'View Info':
+                console.log(`Okay let's View some info!!`);
+                qView()
+                break;
+            default:
+                console.log('See you next time!!');
+                break;
+        }
+    });
+}
+
+
+
+
+    
+const qAdd = () => {
+        
+    console.log(`Ready to add some info??`);
+
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'add',
+            message: 'What would you like to add?',
+            choices: ['Department', 'Role', 'Employee']
+        },
+        {
+            type: 'input',
+            name: 'next',
+            message: 'What can I do next?'
+        }
+    ])
+        .then((addData) => {
+            switch (addData.add) {
+                case 'Department':
+                    addDepartment()
+                    break;
+                case 'Role':
+                    addRole()
+                    break;
+                case 'Employee':
+                    addEmployee()
+                    break;
+            }
+        });
+}
+
+const addDepartment = () => {
+    
+}
+const addRole = () => {
+
+}
+const addEmployee = () => {
+
+}
+
+
+
+
+
+
+
+
+
+
+const qDelete = () => {
+        
+    console.log(`Ready to delete some info??`);
+}
+const qView = () => {
+        
+    console.log(`Ready to view some info??`);
+}
